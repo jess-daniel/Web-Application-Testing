4cc909a42f768783ec7c603cf7688869a00d0db7
Jess Daniel <jessdaniel474@gmail.com>
2019-10-31 17:22:09 -0400

    Added couting logic to dashboard and display

--- app-testing/src/App.js
+++ app-testing/src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import Display from "./components/Display";
+import Dasboard from "./components/Dashboard";
 
 function App() {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [fouls, setFouls] = useState(0);
+
+  const handleHit = () => {
+    setStrikes(0);
+    setBalls(0);
+  };
+
+  const handleBall = () => {
+    setBalls(balls + 1);
+    if (balls === 3) {
+      setBalls(0);
+      setStrikes(0);
+    }
+  };
+
+  const handleStrike = () => {
+    setStrikes(strikes + 1);
+    if (strikes === 2) {
+      setBalls(0);
+      setStrikes(0);
+    }
+  };
+
+  const handleFoul = () => {
+    if (strikes <= 1) {
+      setStrikes(strikes + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display balls={balls} strikes={strikes} />
+      <Dasboard
+        handleHit={handleHit}
+        handleBall={handleBall}
+        handleStrike={handleStrike}
+        handleFoul={handleFoul}
+      />
     </div>
   );
 }
