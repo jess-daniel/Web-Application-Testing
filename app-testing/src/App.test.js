9e8b4993af727a0d154aad0124712127ff3e6416
Jess Daniel <jessdaniel474@gmail.com>
2019-10-31 22:56:52 -0400

    Actually changed  it to test

--- app-testing/src/App.test.js
+++ app-testing/src/App.test.js
@@ -2,21 +2,21 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-it("renders without crashing", () => {
+test("renders without crashing", () => {
   render(<App />);
 });
 
-it("it displays strikes", () => {
+test("it displays strikes", () => {
   const { getByText } = render(<App />);
   getByText(/strikes/i);
 });
 
-it("it has a hit button", () => {
+test("it has a hit button", () => {
   const { getByText } = render(<App />);
   getByText(/hit/i);
 });
 
-it("it has a foul button", () => {
+test("it has a foul button", () => {
   const { getByText } = render(<App />);
   getByText(/foul/i);
 });
